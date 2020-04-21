c9099eb5a15b1484dcd6d48e2f3bfb870db880b7
Calvin Sienatra <sienatra@wisc.edu>
2020-04-21 12:35:16 -0500

    Added main scene

--- application/Main.java
+++ application/Main.java
@@ -1,19 +1,35 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  // Set window properties
+  private static final int WINDOW_WIDTH = 1500;
+  private static final int WINDOW_HEIGHT = 900;
+  private static final String APP_TITLE = "Milk Weight";
+
   @Override
-  public void start(Stage arg0) throws Exception {
-    // TODO Auto-generated method stub
+  public void start(Stage primaryStage) throws Exception {
+    BorderPane root = new BorderPane();
+    
+    Label title = new Label("Milk Weight");
+    root.setTop(title);
+    
+    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+    primaryStage.setTitle(APP_TITLE);
+    primaryStage.setScene(mainScene);
+    primaryStage.show();
 
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    launch(args);
   }
 
 }
