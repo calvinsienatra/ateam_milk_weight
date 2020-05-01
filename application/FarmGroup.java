83a3db6a905e8eb6c227cc050acee02970e50363
cmrkvicka <cmrkvicka@wisc.edu>
2020-04-30 22:38:45 -0500

    Full comments on FarmGroup

--- application/FarmGroup.java
+++ application/FarmGroup.java
@@ -220,7 +220,7 @@ public class FarmGroup {
 
     int totalMilkWeightYear = 0;
 
-
+    // For each correctly formatted farm add milk weight to total
     for (String farmId : filteredFarmIds) {
       totalMilkWeightYear += farms.get(farmId).getMilkWeight(year);
     }
@@ -240,23 +240,28 @@ public class FarmGroup {
     Set<String> farmIds = farms.keySet();
     Set<String> filteredFarmIds = new HashSet<>();
 
+    // For each farm
     for (String farmId : farmIds) {
       try {
+        // Get the milkweight for the year of farms that are formatted correctly and add to filtered
+        // list
         farms.get(farmId).getMilkWeight(year, 1);
         filteredFarmIds.add(farmId);
       } catch (MissingFormatArgumentException e) {
-        // Continue
+        // Continue over farms that aren't correct format
       }
     }
 
     int totalMilkWeightYear = 0;
 
+    // For each filtered farm add milk weight to total
     for (String farmId : filteredFarmIds) {
       totalMilkWeightYear += farms.get(farmId).getMilkWeight(year);
     }
 
     HashMap<String, Double> calculatedPercentage = new HashMap<>();
 
+    // Calculate the percentage of each farm and put it in a HashMap to return
     for (String farmId : filteredFarmIds) {
       int curMilkWeight = farms.get(farmId).getMilkWeight(year);
 
@@ -269,22 +274,34 @@ public class FarmGroup {
     return calculatedPercentage;
   }
 
+  /**
+   * Gets the report of a month for every farm
+   * 
+   * @param year
+   * @param month
+   * @return totalMilkWeightYearMonth
+   * @throws NoSuchElementException - if format of farm year is not correct
+   */
   public int getTotalMilkWeightForAllFarmAndMonth(Integer year, Integer month)
       throws NoSuchElementException {
     Set<String> farmIds = farms.keySet();
     Set<String> filteredFarmIds = new HashSet<>();
 
+    // For each farm
     for (String farmId : farmIds) {
       try {
+        // Get the milkweight for the year of farms that are formatted correctly and add to filtered
+        // list
         farms.get(farmId).getMilkWeight(year, 1);
         filteredFarmIds.add(farmId);
       } catch (MissingFormatArgumentException e) {
-        // Continue
+        // Continue over farms that aren't correct format
       }
     }
 
     int totalMilkWeightYearMonth = 0;
 
+    // For each filtered farm add milkWeight from that month to total
     for (String farmId : filteredFarmIds) {
       totalMilkWeightYearMonth += farms.get(farmId).getMilkWeight(year, month);
     }
@@ -304,23 +321,28 @@ public class FarmGroup {
     Set<String> farmIds = farms.keySet();
     Set<String> filteredFarmIds = new HashSet<>();
 
+    // For each farm
     for (String farmId : farmIds) {
       try {
+        // Get the milkweight for the year of farms that are formatted correctly and add to filtered
+        // list
         farms.get(farmId).getMilkWeight(year, 1);
         filteredFarmIds.add(farmId);
       } catch (MissingFormatArgumentException e) {
-        // Continue
+        // Continue over incorrectly formatted farms
       }
     }
 
     int totalMilkWeightYearMonth = 0;
 
+    // Add milk weight from given month of each filtered farm and add to total
     for (String farmId : filteredFarmIds) {
       totalMilkWeightYearMonth += farms.get(farmId).getMilkWeight(year, month);
     }
 
     HashMap<String, Double> calculatedPercentage = new HashMap<>();
 
+    // Calculate the percentage of each farm milkWeight and put it in a HashMap to return
     for (String farmId : filteredFarmIds) {
       int curMilkWeight = farms.get(farmId).getMilkWeight(year, month);
 
@@ -333,17 +355,30 @@ public class FarmGroup {
     return calculatedPercentage;
   }
 
+
+
+  /**
+   * Get total milk weight for all farms in specific date range
+   * 
+   * @param fromDate - LocalDate object
+   * @param toDate - LocalDate object
+   * @return - totalMilkWeightDateToDate
+   * @throws NoSuchElementException
+   */
   public int getTotalMilkWeightForAllFromDateToDate(LocalDate fromDate, LocalDate toDate)
       throws NoSuchElementException {
     Set<String> farmIds = farms.keySet();
     Set<String> filteredFarmIds = new HashSet<>();
 
+    // For each farm
     for (String farmId : farmIds) {
       try {
+        // Get the milkweight for the start year of farms that are formatted correctly and add to
+        // filtered list
         farms.get(farmId).getMilkWeight(fromDate.getYear(), 1);
         filteredFarmIds.add(farmId);
       } catch (MissingFormatArgumentException e) {
-        // Continue
+        // Continue over incorrectly formatted farms
       }
     }
 
@@ -351,27 +386,30 @@ public class FarmGroup {
 
     HashMap<String, Integer> farmTotal = new HashMap<>();
 
+    // While the date is still within the range
     while (!fromDate.equals(toDate.plusDays(1))) {
-      for (String farmId : filteredFarmIds) {
 
+      for (String farmId : filteredFarmIds) { // For each filtered farm
+
+        // Add the farms milkWeight from each day to the total
         totalMilkWeightDateToDate += farms.get(farmId).getMilkWeight(fromDate.getYear(),
             fromDate.getMonthValue(), fromDate.getDayOfMonth());
 
-        if (farmTotal.get(farmId) == null) {
+        if (farmTotal.get(farmId) == null) { // If the farm isn't in the farmTotal HashMap
+          // Add farm milk weight to farmTotal
           farmTotal.put(farmId, farms.get(farmId).getMilkWeight(fromDate.getYear(),
               fromDate.getMonthValue(), fromDate.getDayOfMonth()));
         } else {
+          // Add farm milk weight to farmTotal from the instance in farmTotal
           farmTotal.put(farmId,
               farmTotal.get(farmId) + farms.get(farmId).getMilkWeight(fromDate.getYear(),
                   fromDate.getMonthValue(), fromDate.getDayOfMonth()));
         }
-
       }
-
-
+      // Increment fromDate for the while loop
       fromDate = fromDate.plusDays(1);
     }
-
+    // Return total milk weight
     return totalMilkWeightDateToDate;
 
   }
@@ -386,19 +424,23 @@ public class FarmGroup {
    */
   public HashMap<String, Double> getDateRangeReport(LocalDate fromDate, LocalDate toDate)
       throws DateTimeException {
-    if (fromDate.compareTo(toDate) > 0) {
+
+    if (fromDate.compareTo(toDate) > 0) { // If the fromDate is after the toDate throw exception
       throw new DateTimeException("Error: dateFrom is more than toDate");
     }
 
     Set<String> farmIds = farms.keySet();
     Set<String> filteredFarmIds = new HashSet<>();
 
+    // For each farm
     for (String farmId : farmIds) {
       try {
+        // Get the milkweight for the start year of farms that are formatted correctly and add to
+        // filtered list
         farms.get(farmId).getMilkWeight(fromDate.getYear(), 1);
         filteredFarmIds.add(farmId);
       } catch (MissingFormatArgumentException e) {
-        // Continue
+        // Continue over incorrectly formatted farms
       }
     }
 
@@ -406,30 +448,35 @@ public class FarmGroup {
 
     HashMap<String, Integer> farmTotal = new HashMap<>();
 
+    // While the date is still within the range
     while (!fromDate.equals(toDate.plusDays(1))) {
+
+      // For each filtered farm
       for (String farmId : filteredFarmIds) {
 
+        // Add the farms milkWeight from each day to the total
         totalMilkWeightDateToDate += farms.get(farmId).getMilkWeight(fromDate.getYear(),
             fromDate.getMonthValue(), fromDate.getDayOfMonth());
 
-        if (farmTotal.get(farmId) == null) {
+        if (farmTotal.get(farmId) == null) {// If the farm isn't in the farmTotal HashMap
+          // Add farm milk weight to farmTotal
           farmTotal.put(farmId, farms.get(farmId).getMilkWeight(fromDate.getYear(),
               fromDate.getMonthValue(), fromDate.getDayOfMonth()));
         } else {
+          // Add farm milk weight to farmTotal from the instance in farmTotal
           farmTotal.put(farmId,
               farmTotal.get(farmId) + farms.get(farmId).getMilkWeight(fromDate.getYear(),
                   fromDate.getMonthValue(), fromDate.getDayOfMonth()));
         }
-
       }
-
-
+      // Increment date for the while loop
       fromDate = fromDate.plusDays(1);
     }
 
 
     HashMap<String, Double> calculatedPercentage = new HashMap<>();
 
+    // Calculate the weight percentage for each farm
     for (String farmId : farmTotal.keySet()) {
       Double curPercentage = (((double) farmTotal.get(farmId)) / totalMilkWeightDateToDate) * 100;
 
